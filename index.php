de1dc1cb62f831187cd287199186282f38324022
Peter Larsson <peter@barschool.net>
2019-02-16 15:03:57 +0700

    Added html doc

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
 
-echo 'Hello from host:' . getenv('HOSTNAME');
-echo "\n\nLatest commit: " . getenv('GIT_COMMIT');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Pipeline POC</title>
+</head>
+<body>
+  <h1>Pipeline POC</h1>
+  <h5>This is host: <i><?php echo getenv('HOSTNAME'); ?></i></h5>
+  <h5>Last commit: <i><?php echo getenv('GIT_COMMIT'); ?></i></h5>
+</body>
+</html>
